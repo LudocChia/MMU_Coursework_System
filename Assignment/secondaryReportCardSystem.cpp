16c46cfc34199d639690f7630640a3baf4dd2760
LauYongPin <1221201382@student.mmu.edu.my>
2024-06-05 22:38:16 +0800

    1

--- Assignment/secondaryReportCardSystem.cpp
+++ Assignment/secondaryReportCardSystem.cpp
@@ -1021,7 +1021,8 @@ void searchBySubject(StudentInfo *students, int studentCount, const string &subj
 
     if (resultsCount > 0)
     {
-        cout << "Results for subject \"" << subject << "\":" << endl;
+        cout << "==============================================================================================" << endl;
+        cout << "                                       SEARCH RESULTS" << endl;
         cout << "==============================================================================================" << endl;
         cout << "| No. | Student ID      | Student Name       | Class    | Subject         | Mark    | Grade  |" << endl;
         cout << "----------------------------------------------------------------------------------------------" << endl;
@@ -1039,7 +1040,12 @@ void searchBySubject(StudentInfo *students, int studentCount, const string &subj
                 cout << "\033[1;32m" << setw(4) << right << fixed << setprecision(1) << student.grades[subjectIndex] << "   ";
             }
             cout << "\033[0m" << " | " << setw(6) << left << getGradeLetter(student.grades[subjectIndex]) << " |" << endl;
+            if (i != resultsCount - 1)
+            {
+                cout << "----------------------------------------------------------------------------------------------" << endl;
+            }
         }
+        cout << "----------------------------------------------------------------------------------------------" << endl;
         cout << "==============================================================================================" << endl;
     }
     else
@@ -1048,6 +1054,7 @@ void searchBySubject(StudentInfo *students, int studentCount, const string &subj
     }
     delete[] results;
 }
+
 
     void ternarySearchMenu()
     {
@@ -1067,6 +1074,7 @@ void searchBySubject(StudentInfo *students, int studentCount, const string &subj
         cout << "[3] Search by Class" << endl;
         cout << "[4] Search by Subject" << endl;
         cout << "[5] Back to Assignment Menu" << endl;
+        cout << "==============================================================================================" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
