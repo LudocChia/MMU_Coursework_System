c23690dff745ce4c37e91780022042f0683db0d2
LudocChia <1221201505@student.mmu.edu.my>
2024-06-15 13:23:09 +0800

    sycn all

--- Assignment/tempCodeRunnerFile.cpp
+++ Assignment/tempCodeRunnerFile.cpp
@@ -1,1 +1,15 @@
-system("pause");
+struct StudentInfo
+{
+    string studentId;
+    string studentName;
+    string className;
+    float grades[5]; // 5 grades for 5 subjects
+
+    StudentInfo()
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            grades[i] = 0.0;
+        }
+    }
+}
